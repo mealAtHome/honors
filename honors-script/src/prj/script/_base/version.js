6b7342639a9962faae986e8c2cc04f8f61cce97f
joonhyung.seo <joonhyung.seo@savaway.co.jp>
2026-05-17 18:42:22 +0900

    릴리즈테스트

--- honors-script/src/prj/script/_base/version.js
+++ honors-script/src/prj/script/_base/version.js
@@ -1,3 +1,4 @@
 // var scriptVersion = new Date('2025-11-09T13:41:00.000Z').getTime();
 // var scriptVersion = new Date().getTime();
-var scriptVersion = 1778593881836;
+var scriptVersion = 1778593881837;
+alert('Script version: ' + scriptVersion);
