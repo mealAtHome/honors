072d17da22c81d4fc37854ee9f46258d7f10d585
joonhyung.seo <joonhyung.seo@savaway.co.jp>
2026-05-24 13:10:16 +0900

    릴리즈 준비

--- honors-script/src/prj/script/_base/version.js
+++ honors-script/src/prj/script/_base/version.js
@@ -1,4 +1,4 @@
 // var scriptVersion = new Date('2025-11-09T13:41:00.000Z').getTime();
 // var scriptVersion = new Date().getTime();
-var scriptVersion = 1779104274838;
+var scriptVersion = 1779595478672;
 // alert('Script version: ' + scriptVersion);
